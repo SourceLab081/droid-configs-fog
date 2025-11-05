c3823b55ae501aed335366d63d9fb22f29916966
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-05 20:42:47 +0700

    Disable soundfx library mounts in early init script
    Comment out soundfx library mount commands.

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -4,7 +4,7 @@
 ln -s /usr/lib64/pulse-14.2/modules/libdroid-util.so /usr/lib64/pulseaudio/libdroid-util.so
 # Make pulseaudio happy!
 # mount --bind /vendor/lib64/libqtigef.so /vendor/lib/libqtigef.so
-mount --bind /vendor/lib64/soundfx/libqcompostprocbundle.so /vendor/lib/soundfx/libqcompostprocbundle.so
-mount --bind /vendor/lib64/soundfx/libqcomvisualizer.so /vendor/lib/soundfx/libqcomvisualizer.so
+# mount --bind /vendor/lib64/soundfx/libqcompostprocbundle.so /vendor/lib/soundfx/libqcompostprocbundle.so
+# mount --bind /vendor/lib64/soundfx/libqcomvisualizer.so /vendor/lib/soundfx/libqcomvisualizer.so
 
 sh /usr/bin/droid/stune-fix.sh

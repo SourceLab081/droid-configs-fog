185236a6377bc225c150e38bcee3bc340910751a
debian <console_cloud_google_com@localhost>
2026-05-07 10:13:28 +0000

    update

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mount  --bind /vendor/lib64/soundfx/libqcomvisualizer.so /vendor/lib/soundfx/libqcomvisualizer.so
-mount  --bind /vendor/lib64/soundfx/libqcompostprocbundle.so /vendor/lib/soundfx/libqcompostprocbundle.so
-mount  --bind /vendor/lib64/libadm.so /vendor/lib/libadm.so
-mount  --bind /vendor/lib64/hw/sound_trigger.primary.bengal.so /vendor/lib/hw/sound_trigger.primary.bengal.so
-mount  --bind /vendor/lib64/libqtigef.so /vendor/lib/libqtigef.so
+mkdir -p /vendor /odm/etc /product /system_ext /mnt/vendor/persist /odm_root
+mount --bind /odm_root/etc /odm/etc
+#mount  --bind /vendor/lib64/soundfx/libqcomvisualizer.so /vendor/lib/soundfx/libqcomvisualizer.so
+#mount  --bind /vendor/lib64/soundfx/libqcompostprocbundle.so /vendor/lib/soundfx/libqcompostprocbundle.so
+#mount  --bind /vendor/lib64/libadm.so /vendor/lib/libadm.so
+#mount  --bind /vendor/lib64/hw/sound_trigger.primary.bengal.so /vendor/lib/hw/sound_trigger.primary.bengal.so
+#mount  --bind /vendor/lib64/libqtigef.so /vendor/lib/libqtigef.so
 # bind custom patches
 #mount --bind $lipstick_patch /usr/share/lipstick-jolla-home-qt5/statusarea/StatusArea.qml
 
